9533725e9793013c79bc797074842862b4b79c42
neal.nankivell <neal.nankivell@gmail.com>
2019-09-04 12:53:34 +0200

    Implmented TopKFrequent selection using a min heap
    This was quite intersting to get my head around,
    and I also feel my imlmentation is quite messy :(
    
    The idea is to maintain a min heap of length k,
    allowing us to easily compare each item with the head
    if the heap and swap it if it is larger - then bubble it into
    the correct position.
    
    Interestingly (and shouldnt be too much of a suprise)
    this is not magnitudes better than my previous linq solution
    when submitting on leetcode. linq's `.OrderByDescending()`
    and underpinning OrderedEnumerable perform about the
    same and is significantly more readable.

--- Answer.Test/SolutionTests.cs
+++ Answer.Test/SolutionTests.cs
@@ -16,6 +16,16 @@ namespace Tests
             1,
             new[] { 1 }
         )]
+        [TestCase(
+            new[] { 5, 2, 5, 3, 5, 3, 1, 1, 3 },
+            2,
+            new[] { 3, 5 }
+        )]
+        [TestCase(
+            new[] { 4, 1, -1, 2, -1, 2, 3 },
+            2,
+            new[] { -1, 2 }
+        )]
         public void TopKFrequent(int[] nums, int k, int[] expectedResult)
         {
             var sut = new Solution();
--- Answer/Solution.cs
+++ Answer/Solution.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Answer
@@ -27,24 +26,113 @@ namespace Answer
          */
         public IList<int> TopKFrequent(int[] nums, int k)
         {
-            var frequencyMap = new Dictionary<int, int>();
+            Dictionary<int, Item> map = new Dictionary<int, Item>();
             foreach (int num in nums)
             {
-                if (frequencyMap.ContainsKey(num))
+                if (map.TryGetValue(num, out Item item))
                 {
-                    frequencyMap[num]++;
+                    item.Frequency++;
                 }
                 else
                 {
-                    frequencyMap[num] = 1;
+                    map[num] = new Item(num);
                 }
             }
 
-            return frequencyMap
-                .OrderByDescending(kvp => kvp.Value)
-                .Take(k)
-                .Select(kvp => kvp.Key)
-                .ToList();
+            return TopKFrequent(map.Values, k);
+        }
+
+        private IList<int> TopKFrequent(IEnumerable<Item> items, int k)
+        {
+            List<Item> minHeap = new List<Item>();
+
+            void AddToMinHeap(Item item)
+            {
+                if (minHeap.Count == k)
+                {
+                    if (item.Frequency > minHeap[0].Frequency)
+                    {
+                        AddToStartAndBubbleDown(item);
+                    }
+                }
+                else
+                {
+                    AddToEndAndBubbleUp(item);
+                }
+            }
+
+            void AddToStartAndBubbleDown(Item item)
+            {
+                var index = 0;
+                minHeap[index] = item;
+
+                var childIndexA = (index * 2) + 1;
+                var childIndexB = (index * 2) + 2;
+
+                var childItemA = childIndexA < minHeap.Count ? minHeap[childIndexA] : null;
+                var childItemB = childIndexB < minHeap.Count ? minHeap[childIndexB] : null;
+
+                while (childItemA != null && childItemA.Frequency < item.Frequency ||
+                    childItemB != null && childItemB.Frequency < item.Frequency)
+                {
+                    bool swapWithA = childItemB == null || childItemA.Frequency < childItemB.Frequency;
+                    int indexToSwap = swapWithA ? childIndexA : childIndexB;
+                    Item itemToSwap = swapWithA ? childItemA : childItemB;
+
+                    minHeap[index] = itemToSwap;
+                    minHeap[indexToSwap] = item;
+
+                    index = indexToSwap;
+
+                    childIndexA = (index * 2) + 1;
+                    childIndexB = (index * 2) + 2;
+                    childItemA = childIndexA < minHeap.Count ? minHeap[childIndexA] : null;
+                    childItemB = childIndexB < minHeap.Count ? minHeap[childIndexB] : null;
+                }
+            }
+
+            void AddToEndAndBubbleUp(Item item)
+            {
+                var index = minHeap.Count;
+                minHeap.Add(item);
+                var parentIndex = GetParentIndex(index);
+                while (parentIndex.HasValue)
+                {
+                    var parentItem = minHeap[parentIndex.Value];
+                    if (parentItem.Frequency <= item.Frequency)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        minHeap[parentIndex.Value] = item;
+                        minHeap[index] = parentItem;
+                        index = parentIndex.Value;
+                        parentIndex = GetParentIndex(index);
+                    }
+                }
+            }
+
+            int? GetParentIndex(int index) =>
+                (index == 0) ? (int?)null : (index - 1) / 2;
+
+            foreach (Item item in items)
+            {
+                AddToMinHeap(item);
+            }
+            return minHeap.Select(item => item.Value).ToList();
+
+        }
+
+        private class Item
+        {
+            public Item(int value)
+            {
+                Value = value;
+                Frequency = 1;
+            }
+            public int Value { get; }
+            public int Frequency { get; set; }
         }
     }
 }
